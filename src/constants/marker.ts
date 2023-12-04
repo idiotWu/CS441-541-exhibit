2573c8cdd21785261cf772742e0ee5e82ee24c2e
Daofeng Wu <dolphin.w.e@gmail.com>
2023-12-03 23:01:02 -0500

    chore: update descriptions

--- src/constants/marker.ts
+++ src/constants/marker.ts
@@ -12,6 +12,8 @@ export const eventMarkers: EventMarker[] = [
 _To: Emory University, Georgia Tech, Georgia State University_
 
 We demand these universities end all financial support to the Atlanta Police Foundation and Cop City!
+
+Black communities will be especially harmed by the police expansion and militarization that Cop City represents . Studies have shown that police with more military equipment kill more civilians, prevent less crime, and make residents feel less safe., The fake urban village APF seeks to create will train officers to view Atlanta like a warzone and treat civilians like potential enemy combatants. Let’s not mince words: The Atlanta Police Foundation has a history of using corporate donations to fund the expansion of the police at the expense of the Black community in Atlanta. Universities are key funders for Cop City despite their public commitments towards racial justice. Emory, GSU, and Georgia Tech must pick a side. They cannot stand with us if they continue to fund Cop City.
 `,
     longitude: -84.4070478,
     latitude: 33.7334532,
@@ -23,8 +25,9 @@ We demand these universities end all financial support to the Atlanta Police Fou
     video: 'videos/DSA.mp4',
     organization: 'Atlanta DSA',
     url: 'https://www.atldsa.org/join/',
-    description:
-      'Atlanta DSA is an entirely volunteer-run, member-driven organization. We make decisions democratically in our monthly membership meetings, which are open to all. ',
+    description: `
+DSA is a volunteer group with many different programs. We campaign to win reforms that build working-class power by pressuring elected officials. We support the labor movement with public awareness campaigns, solidarity actions, and direct organizing. We’re creating working-class consciousness with our reading groups and educational events. As a democratic, grassroots organization, DSA is building a mass movement for political revolution. By paying dues, DSA members self-fund our own tools for liberation.
+    `,
     longitude: -84.4107494,
     latitude: 33.7261626,
     color: '#ec1f27',
@@ -57,8 +60,9 @@ Join the SCHR for a discussion with formerly incarcerated leaders about ending m
     video: 'videos/EMI.mp4',
     organization: 'EMI Georgia',
     url: 'https://columbiauniversity.zoom.us/webinar/register/WN_A1n4JSLZTlmJ5apzj6HZFA#/registration',
-    description:
-      'Based on interviews conducted with low-income Black mothers in Washington, DC, the talk will explore the precarious status of motherhood under conditions of racially unequal systems of surveillance by prisons, police, and child welfare.',
+    description: `
+Based on interviews conducted with low-income Black mothers in Washington, DC, the talk will explore the precarious status of motherhood under conditions of racially unequal systems of surveillance by prisons, police, and child welfare. While most debate over police reform focuses on the carceral system's perils for Black men, this discussion responds to and produces particular perils for Black women. To tell these mothers' stories and to advance the project of narrative shifting, the talk uses the tool of "empirical poetry"--poems constructed from the narratives of these women.
+    `,
     longitude: -84.3022857,
     latitude: 33.7463465,
     color: '#c8d700',
@@ -69,8 +73,11 @@ Join the SCHR for a discussion with formerly incarcerated leaders about ending m
     video: 'videos/stop-cop-city.mov',
     organization: 'Stop Cop City!',
     url: 'https://stopcop.city/',
-    description:
-      "Atlanta's ruling class wants to demolish our public forests to build a $90,000,000 Cop City. Let's stop them!",
+    description: `
+Atlanta Police Foundation is trying to build the largest police training facility in the US in Weelaunee Forest, a watershed surrounded by primarily Black residents who overwhelmingly oppose the project.
+
+The plans include military-grade training facilities, a mock city to practice urban warfare, dozens of shooting ranges, and a Black Hawk helicopter landing pad.
+    `,
     longitude: -84.3988567,
     latitude: 33.7504676,
     color: '#474662',
